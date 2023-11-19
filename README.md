ed98703105e72b2e35c6d510529e792df3905aa1
Seth Stephanz <sethstephanz@gmail.com>
2023-11-18 19:45:45 -0500

    Update logic and readme

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # 361-microservice
 
 Microservice for partner's project for CS 361:
-I am implementing a microservice that communicates with HTTP Requests (RESTful API). It will retrieve user-submitted text and analyze the text by using natural language processing techniques such as RAKE to find the keywords which will be used in Brooke's hashtag generator website.
+I am implementing a microservice that communicates with HTTP Requests (RESTful API). It will retrieve user-submitted text and analyze the text by using natural language processing techniques such as RAKE to find the keywords which will be used in Brooke's hashtag generator website. It will then provide several hashtags in response.
+
+To use my API microservice:
+
+1. Stringify your target text.
+2. Send a POST request to my API (hosted at https://cs-361-microservice-405500.wl.r.appspot.com/hashtaggenerator). The POST request's body should be of the form: {"content": "<Target text in string format>"}
+3. The API will send back 5 hashtags of the most commonly occurring nouns in the text.
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-# citation: https://realpython.com/nltk-nlp-python/#getting-started-with-pythons-nltk
-# https://stackoverflow.com/questions/15547409/how-to-get-rid-of-punctuation-using-nltk-tokenizer#:~:text=The%20workflow%20assumed%20by%20NLTK,or%20python's%20isalnum()%20function.&text=It%20does%20work%3A%20%3E%3E%3E%20'with%20dot.
+# citation: Overview of NLTK and NLTK methods: https://realpython.com/nltk-nlp-python/#getting-started-with-pythons-nltk
+# For help on getting rid of punctuation: https://stackoverflow.com/questions/15547409/how-to-get-rid-of-punctuation-using-nltk-tokenizer#:~:text=The%20workflow%20assumed%20by%20NLTK,or%20python's%20isalnum()%20function.&text=It%20does%20work%3A%20%3E%3E%3E%20'with%20dot.
 # text sources are all public domain work gathered from: https://www.gutenberg.org/
+# API skeleton and logic based on code acquired from https://canvas.oregonstate.edu/courses/1933801/pages/exploration-google-app-engine-and-python?module_item_id=23529288
+# https://towardsdatascience.com/how-to-transform-technical-jargon-into-simple-bi-tri-grams-with-nlp-on-a-public-dataset-2081f5609c1f
 
 # Looked at this, although my strategy is a little different:
 # https://thawuship.medium.com/mastering-hashtag-generation-with-pythons-natural-language-processing-techniques-6e093660c2f3
@@ -14,16 +16,16 @@ from nltk.stem import WordNetLemmatizer
 from nltk import FreqDist
 import json
 from flask import Flask, request
-from google.cloud import datastore
+# from google.cloud import datastore
 import nltk
 # you will need to download these from nltk once per program on your local machine:
-# nltk.download('punkt')
-# nltk.download("stopwords")
-# nltk.download('wordnet')
-
+nltk.download('punkt')
+nltk.download("stopwords")
+nltk.download('wordnet')
+nltk.download("averaged_perceptron_tagger")
 
 app = Flask(__name__)
-client = datastore.Client()
+# client = datastore.Client()
 
 
 @app.route('/')
@@ -39,15 +41,17 @@ def boats_get_post():
         # don't know where 'wa' is coming from, but is always added, so remove explicitly
         stop_words.append('wa')
 
-        # text = open('text.txt', 'r')
-        # content = text.read()
-        # text.close()
-
         # print('request:', request.json())
-        content = request.get_json()['content']
-        print('content:', content)
 
-        # print('stop words:', stop_words)
+        # content = nltk.pos_tag(content)
+        # grammar = "NP: {<DT>?<JJ>*<NN>}"
+        # chunk_parser = nltk.RegexpParser(grammar)
+        # parsed = chunk_parser.parse(content)
+        # print('parsed:', parsed)
+
+        # get content out of request and tokenize
+        content = request.get_json()['content']
+
         # tokenize content while removing punctuation
         # content = word_tokenize(content)
         tokenizer = RegexpTokenizer(r'\w+')
@@ -62,27 +66,32 @@ def boats_get_post():
         meaningfulWords = [
             word for word in lemmatized_words if word.casefold() not in stop_words]
 
-        # now, produce frequency distribution
-        freqency_distribution = FreqDist(meaningfulWords)
-        mostCommon10 = freqency_distribution.most_common(10)
+        # tag words in meaningfulWords for grammatical purpose
+        # only really want to create hashtags out of nouns
+        # tags = nltk.pos_tag(meaningfulWords) # with lemmatization
+        tags = nltk.pos_tag(content)    # without lemmatization
+        # keywords = [word for (word, tag) in tags if tag.startswith(
+        #     'NN') or tag == 'JJ' or tag == 'NNP']
+        # keywords = [word for (word, tag) in tags if tag.startswith('NN') or tag == 'NNP']
+        keywords = [word for (word, tag) in tags if tag.startswith(
+            'NN') or tag == 'NNS']
+        print('tags:', keywords)
 
-        commonWords = []
+        # now, produce frequency distribution
+        freqency_distribution = FreqDist(keywords)
+        mostCommon10 = freqency_distribution.most_common(5)
+
+        hashTags = []
 
         for item in mostCommon10:
-            print('#' + item[0])
-            commonWords.append(item)
-
-        returnArr = []
+            hashTagItem = ('#' + item[0])
+            hashTags.append(hashTagItem)
 
         # print ten most common meaningful words(hashtags)
         # print('freq dist:', freqency_distribution.most_common(10))
-        for item in mostCommon10:
-            # print(item)
-            # item = list(item)
-            # this should get the word from the tuple
-            returnArr.append(item[0])
-        # return ({returnObject}, 200)
-        return ({'10 Most Common Words: ': returnArr}, 200)
+        # for item in mostCommon10:
+        #     returnArr.append(item[0])
+        return ({'Hash Tags: ': hashTags}, 200)
     else:
         return ({"Error": "Method not found!"}, 404)
 
